330e3a801880cce7b1940e41587ac13d121d6e04
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-11-13 11:12:03 +0800

    add

--- Assets/Cartoon/Shaders/SSAO.hlsl
+++ Assets/Cartoon/Shaders/SSAO.hlsl
@@ -109,8 +109,8 @@
 	
 	inline float UVRandom(float2 u, float v)
 	{
-		float f = dot(float2(12.9898, 78.233), float2(u, v));
-		return frac(43758, 5453 * sin(f));
+		float f = dot(float2(12.9898, 78.233), float2(u.x, v));
+		return frac(43758.5453 * sin(f));
 	}
 	
 	/*
@@ -128,9 +128,11 @@
 	float3 PickSamplePoint(float2 uv, float randAddon, int index)
 	{
 		float2 positionSS = GetScreenSpacePosition(uv);
-		float gn = InterleaveGradientNoise(positionSS, index);
+		float gn = InterleavedGradientNoise(positionSS, index);
+		//[-1,1]
 		float u = frac(UVRandom(0.0, index + randAddon) + gn) * 2.0 - 1.0;
 		float theta = (UVRandom(1.0, index + randAddon) + gn) * TWO_PI;
+		//float3([-1,1] * v , u)  =>  [-1,1]
 		return float3(CosSin(theta) * sqrt(1.0 - u * u), u);
 	}
 	
@@ -162,10 +164,11 @@
 		//采样深度
 		//非线性
 		float rawDepth = SampleSceneDepth(uv.xy).r;
-		//转换到线性
+		//转换到线性 [0,1]
 		return RawToLinearDepth(rawDepth);
 	}
 	
+	//float3(uv.xy * screen.size , depth)
 	float3 ReconstructViewPos(float2 uv, float depth, float2 p11_22, float2 p13_31)
 	{
 		//p13_31 多半是00
@@ -179,7 +182,7 @@
 			//depth是z * tan  得到屏幕屏幕宽高  * uv   得出最后位置
 			float3 viewPos = float3(depth * ((uv.xy * 2.0 - 1.0 - p13_31) * p11_22), depth);
 		#endif
-		return viewpos;
+		return viewPos;
 	}
 	
 	
@@ -189,6 +192,7 @@
 	// High:   5 taps on each direction: | z | x | * | y | w |
 	// https://atyuwen.github.io/posts/normal-reconstruction/
 	// https://wickedengine.net/2019/09/22/improved-normal-reconstruction-from-depth/
+	// low ddx/ddy的最近depth方向    !=low 周围最近点的深度转成方向
 	float3 ReconstructNormal(float2 uv, float depth, float3 vpos, float2 p11_22, float2 p13_31)
 	{
 		#if defined(_RECONSTRUCT_NORMAL_LOW)
@@ -202,17 +206,16 @@
 			float2 uUV = float2(0.0, delta.y);
 			float2 dUV = float2(0.0, -delta.y);
 			
-			float3 l1 = float3(uv + lUV, 0.0); 
+			float3 l1 = float3(uv + lUV, 0.0);
 			l1.z = SampleAndGetLinearDepth(l1.xy); // Left1
-			float3 r1 = float3(uv + rUV, 0.0); 
+			float3 r1 = float3(uv + rUV, 0.0);
 			r1.z = SampleAndGetLinearDepth(r1.xy); // Right1
-			float3 u1 = float3(uv + uUV, 0.0); 
+			float3 u1 = float3(uv + uUV, 0.0);
 			u1.z = SampleAndGetLinearDepth(u1.xy); // Up1
-			float3 d1 = float3(uv + dUV, 0.0); 
+			float3 d1 = float3(uv + dUV, 0.0);
 			d1.z = SampleAndGetLinearDepth(d1.xy); // Down1
 			
-			//TODO:hcs
-
+			
 			// Determine the closest horizontal and vertical pixels...
 			// horizontal: left = 0.0 right = 1.0
 			// vertical  : down = 0.0    up = 1.0
@@ -220,13 +223,13 @@
 				uint closest_horizontal = l1.z > r1.z ? 0: 1;
 				uint closest_vertical = d1.z > u1.z ? 0: 1;
 			#else
-				float3 l2 = float3(uv + lUV * 2.0, 0.0); 
+				float3 l2 = float3(uv + lUV * 2.0, 0.0);
 				l2.z = SampleAndGetLinearDepth(l2.xy); // Left2
-				float3 r2 = float3(uv + rUV * 2.0, 0.0); 
+				float3 r2 = float3(uv + rUV * 2.0, 0.0);
 				r2.z = SampleAndGetLinearDepth(r2.xy); // Right2
-				float3 u2 = float3(uv + uUV * 2.0, 0.0); 
+				float3 u2 = float3(uv + uUV * 2.0, 0.0);
 				u2.z = SampleAndGetLinearDepth(u2.xy); // Up2
-				float3 d2 = float3(uv + dUV * 2.0, 0.0); 
+				float3 d2 = float3(uv + dUV * 2.0, 0.0);
 				d2.z = SampleAndGetLinearDepth(d2.xy); // Down2
 				
 				const uint closest_horizontal = abs((2.0 * l1.z - l2.z) - depth) < abs((2.0 * r1.z - r2.z) - depth) ? 0: 1;
@@ -275,7 +278,7 @@
 		#endif
 	}
 	
-	float3x3 GetCoordinateConversionParameters(out float2 p11_22, out float p13_31)
+	float3x3 GetCoordinateConversionParameters(out float2 p11_22, out float2 p13_31)
 	{
 		float3x3 camProj = (float3x3)unity_CameraProjection;
 		
@@ -301,6 +304,57 @@
 		float3 norm_o;
 		float3 vpos_o;
 		SampleDepthNormalView(uv, p11_22, p13_31, depth_o, norm_o, vpos_o);
+		
+		float randAddon = uv.x * 1e-10;
+		float rcpSampleCount = rcp(SAMPLE_COUNT);
+		float ao = 0.0;
+		for (int s = 0; s < int(SAMPLE_COUNT); s ++)
+		{
+			#if defined(SHADER_API_D3D11)
+				//DX11 NVidia问题
+				s = floor(1.0001 * s);
+			#endif
+			
+			//随机采样点[-1,1]
+			float3 v_s1 = PickSamplePoint(uv, randAddon, s);
+			
+			//[-1,1] * [0,_Radius]
+			v_s1 *= sqrt((s + 1.0) * rcpSampleCount) * RADIUS;
+			
+			//faceforward(n, i, ng)    return -n * sign(dot(i, ng))  强制面向正面
+			v_s1 = faceforward(v_s1, -norm_o, v_s1);
+			float3 vpos_s1 = vpos_o + v_s1;
+			
+			float3 spos_s1 = mul(camProj, vpos_s1);
+			#if defined(_ORTHOGRAPHIC)
+				//[-1,1]=>[0,1]
+				float2 uv_s1_01 = clamp((spos_s1.xy + 1.0) * 0.5, 0.0, 1.0);
+			#else
+				//齐次对齐 重新校对
+				float2 uv_s1_01 = clamp((spos_s1.xy * rcp(vpos_s1.z) + 1.0) * 0.5, 0.0, 1.0);
+			#endif
+			
+			//重新转换为正确的点
+			float depth_s1 = SampleAndGetLinearDepth(uv_s1_01);
+			float3 vpos_s2 = ReconstructViewPos(uv_s1_01, depth_s1, p11_22, p13_31);
+			float3 v_s2 = vpos_s2 - vpos_o;
+			
+			//AO由dot（原点，偏移点）的角度决定 ， 因为dot>0 既在离屏幕近  则是ao
+			//kBeta * depth_o 起抑制作用
+			float a1 = max(dot(v_s2, norm_o) - kBeta * depth_o, 0.0);
+			//随着距离变大而减少
+			float a2 = dot(v_s2, v_s2) + EPSILON;
+			ao += a1 * rcp(a2);
+		}
+		
+		//因为之前radius越大 则距离越大 衰减越厉害
+		//这里乘回来
+		ao *= RADIUS;
+		
+		//PositivePow(base,power) pow(abs(base), power)
+		ao = PositivePow(ao * INTENSITY * rcpSampleCount, kContrast);
+		
+		return PackAONormal(ao, norm_o);
 	}
 	
 #endif
