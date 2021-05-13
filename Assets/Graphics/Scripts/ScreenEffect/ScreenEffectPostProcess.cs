3f88a93a5287d2ddb15075e9e8f463221bc8aaee
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-05-13 14:28:38 +0800

    add

--- Assets/Graphics/Scripts/ScreenEffect/ScreenEffectPostProcess.cs
+++ Assets/Graphics/Scripts/ScreenEffect/ScreenEffectPostProcess.cs
@@ -6,15 +6,36 @@ using UnityEngine.Rendering.Universal;
 
 namespace Graphics.Scripts.ScreenEffect
 {
+	[Serializable]
+	public sealed class RenderPassEventParameter : VolumeParameter<RenderPassEvent>
+	{
+		public RenderPassEventParameter(RenderPassEvent value, bool overrideState = false)
+			: base(value, overrideState)
+		{
+		}
+	}
+
+	[Serializable]
+	public sealed class MaterialParameter : VolumeParameter<Material>
+	{
+		public MaterialParameter(Material value, bool overrideState = false)
+			: base(value, overrideState)
+		{
+		}
+	}
 
 	[Serializable, VolumeComponentMenu("My/ScreenEffect")]
 	public class ScreenEffectPostProcess : VolumeComponent, IPostProcessComponent
 	{
 		public BoolParameter enableEffect = new BoolParameter(false);
 
+		public RenderPassEventParameter renderPassEvent =
+			new RenderPassEventParameter(RenderPassEvent.BeforeRenderingPostProcessing);
+
+		public MaterialParameter effectMat = new MaterialParameter(null);
+
 		public bool IsActive() => enableEffect.value;
 
 		public bool IsTileCompatible() => false;
-		
 	}
 }
